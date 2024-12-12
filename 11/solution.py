3f1e0426306b3376925d070c1b34aec1e9946a1d
Fernando Chorney <github@djsbx.com>
2024-12-12 10:49:37 -0600

    Solved day 11

--- 11/solution.py
+++ 11/solution.py
@@ -1,59 +1,50 @@
+from collections import Counter, defaultdict
+from functools import cache
 from pathlib import Path
 
-from icecream import ic
-
 from libaoc import solve
+
+STONE_T: type = list[int]
 
 
 def solution1(input_path: Path) -> int:
     stones = parse_input(input_path)
-    ic(stones)
-
-    for _ in range(25):
-        idx = 0
-        while idx < len(stones):
-            idx = blink(stones, idx) + 1
-    return len(stones)
+    return process_stones(stones, 25)
 
 
 def solution2(input_path: Path) -> int:
     stones = parse_input(input_path)
-    ic(stones)
+    return process_stones(stones, 75)
 
-    count = 0
-    idx = 0
 
-    for _ in range(25):
-        pass
+# I'm ashamed to say I caved after not being able to get this running in a reasonable time.
+# Grabbed the idea for this from: https://github.com/brass75/AdventOfCode/blob/main/aoc_2024/day11.py
+def process_stones(stones: STONE_T, iterations: int) -> int:
+    counts = Counter(stones)
+    for _ in range(iterations):
+        results = defaultdict(int)
+        for stone, count in counts.items():
+            for transformed in transform(stone):
+                results[transformed] += count
+        counts = results
+    return sum(counts.values())
 
-    return -1
 
-def transform(stone: int) -> tuple[int, ...]:
+@cache
+def transform(stone: int) -> STONE_T:
+    if stone == 0:
+        return [1]
+
     stone_str = str(stone)
     stone_len = len(stone_str)
+    if stone_len % 2 == 0:
+        split = stone_len // 2
+        return [int(stone_str[:split]), int(stone_str[split:])]
 
-    if stone == 0:
-        return (1,)
-    elif stone_len % 2 == 0:
-        left = int(stone_str[:stone_len // 2])
-        right = int(stone_str[stone_len // 2:])
-        return (left, right,)
-
-    return (stone * 2024,)
-
-def blink(stones: list[int], idx: int) -> int:
-    result = transform(stones[idx])
-
-    if len(result) == 1:
-        stones[idx] = result[0]
-        return idx
-
-    stones[idx] = result[0]
-    stones.insert(idx + 1, result[1])
-    return idx + 1
+    return [stone * 2024]
 
 
-def parse_input(input_path: Path) -> list[int]:
+def parse_input(input_path: Path) -> STONE_T:
     data: list[int] = []
     with input_path.open() as f:
         for line in map(str.strip, f.readlines()):
@@ -63,5 +54,5 @@ def parse_input(input_path: Path) -> list[int]:
 
 if __name__ == '__main__':
     answer1 = 185894
-    answer2 = -999
+    answer2 = 221632504974231
     solve(solution1, solution2, (answer1, answer2))
